c642191952c70f51a1af05e920c64de59955763c
jyothika-kj <jyothikakj20@gmail.com>
2026-06-27 20:45:50 +0530

    Fix CORS property name

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origin_list,
+    allow_origins=settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
